a71fb3ef8daf6fc748fc1c57f9ceac3e3ee9d199
JoaopedroSantana1 <joaopedrosantanarosa@gmail.com>
2023-11-16 12:46:08 -0300

    update.pdf

--- Js/script.js
+++ Js/script.js
@@ -1,10 +1,10 @@
 document.getElementById("link-pdf").addEventListener("click", function(event) {
   
-    var pdfPath = "/pdf/Joao-PDF.pdf";
+    var pdfPath = "/pdf/joao-curriculo.pdf";
     
     var link = document.createElement("a");
     link.href = pdfPath;
-    link.download = "Joao-PDF.pdf"; 
+    link.download = "joao-curriculo.pdf"; 
     
     link.click();
     event.preventDefault();
