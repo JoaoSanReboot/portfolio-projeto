f8f5b874ab4057272b0a6e25cada0fad91f7b589
João Pedro <joaopedrosantanarosa@gmail.com>
2025-05-18 14:36:56 -0300

    CurriculoAtualizado

--- Js/script.js
+++ Js/script.js
@@ -1,6 +1,6 @@
 document.getElementById("link-pdf").addEventListener("click", function(event) {
   
-    var pdfPath = "/pdf/Joao-Curriculo.pdf";
+    var pdfPath = "/pdf/CurriculoJoaoPedro.pdf";
     
     var link = document.createElement("a");
     link.href = pdfPath;
